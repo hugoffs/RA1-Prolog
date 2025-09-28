368f14aea1ff60f48cd468a9d2306f33f039a417
Angelo Andrioli <angelonetho@gmail.com>
2025-09-27 23:44:15 -0300

    Add `tests/perfil_teste_2.pl` for testing web development profile

---
+++ tests/perfil_teste_2.pl
@@ -0,0 +1,16 @@
+/*
+swipl
+?- consult('src/base_conhecimento.pl').
+?- consult('src/regras.pl').
+?- consult('tests/perfil_teste_2.pl').
+?- executar_teste.
+*/
+
+% Perfil: Aluno com inclinação para desenvolvimento web.
+resposta(1, n).
+resposta(2, s).
+resposta(3, s).
+resposta(4, n).
+resposta(5, n).
+resposta(6, s).
+resposta(7, n).
