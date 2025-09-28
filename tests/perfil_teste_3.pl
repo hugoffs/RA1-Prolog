e1f1908a896e4fa5be55376d89c32076b67b4842
Angelo Andrioli <angelonetho@gmail.com>
2025-09-27 23:49:49 -0300

    Add `tests/perfil_teste_3.pl` for testing networks and infrastructure profile

---
+++ tests/perfil_teste_3.pl
@@ -0,0 +1,16 @@
+/*
+swipl
+?- consult('src/base_conhecimento.pl').
+?- consult('src/regras.pl').
+?- consult('tests/perfil_teste_3.pl').
+?- executar_teste.
+*/
+
+% Perfil: inclinação para redes e infraestrutura
+resposta(1, n).
+resposta(2, n).
+resposta(3, n).
+resposta(4, s).
+resposta(5, n).
+resposta(6, s).
+resposta(7, s).
